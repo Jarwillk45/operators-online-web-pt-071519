3671f80197f84a6d1f2c7825d43a03ce29ef22cf
Jarred Wilkins <wilkins.jarred@outlook.com>
2019-07-20 07:28:11 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
 def unsafe?(speed)
-    speed< 40 or > 60 ? true: false
+    speed<40 or>60 ? true: false
   
 end
 
 
 
 def not_safe?(speed)
-    speed< 40 or >60 ? true: false
+    speed<40 or>60 ? true: false
 end
 	
 
